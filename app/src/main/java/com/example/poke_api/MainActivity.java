9d0346c6106594769b86b7ca323f9d1f4c5934d9
dajdCUE <116434111+dajdCUE@users.noreply.github.com>
2022-11-22 14:20:10 +0100

    Api implement

--- app/src/main/java/com/example/poke_api/MainActivity.java
+++ app/src/main/java/com/example/poke_api/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.poke_api;
 import static com.example.poke_api.utils.Constants.DEBUG_POKEMON;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +12,11 @@ import android.util.Log;
 import com.example.poke_api.models.Pokemon;
 import com.example.poke_api.network.PokeApi;
 import com.example.poke_api.responses.PokemonResponse;
+import com.example.poke_api.ui.PokemonAdapter;
+import com.example.poke_api.utils.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,10 +26,33 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+    // UI elements (fuera del onCreate)
+    private RecyclerView rvPokemon;
+    private PokemonAdapter pokemonAdapter;
+    private List<Pokemon> pokemonList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Declaración y asociación de objetos base (dentro del onCreate)
+        pokemonList = new ArrayList<Pokemon>();
+        rvPokemon = findViewById(R.id.rvPokemon);
+
+        // Configurar Adapter (dentro del onCreate)
+        pokemonAdapter = new PokemonAdapter(pokemonList, MainActivity.this);
+
+        // Configurar RecyclerView
+        rvPokemon.setAdapter(pokemonAdapter);
+        rvPokemon.setHasFixedSize(true);
+
+        // Crear un gestor de layout para el RecyclerView (será vertical y lineal simple)
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+
+        // Introducir el LayoutManager generado al RecyclerView
+        rvPokemon.setLayoutManager(layoutManager);
 
         String baseUrl="https://pokeapi.co/api/v2/";
 
@@ -32,6 +62,16 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
+        // Cargar pokemons en el RecyclerView (lo último)
+        loadPokemons(retrofit);
+
+
+
+
+
+    }
+
+    private void loadPokemons(Retrofit retrofit) {
         //Instanciamos la API de pokemon
         PokeApi api = retrofit.create(PokeApi.class);
         Call<PokemonResponse> call = api.getPokemonList();
@@ -42,17 +82,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<PokemonResponse> call, Response<PokemonResponse> response) {
                 if(response.isSuccessful()){
-//                    assert response.body() != null;
-                    for(int i=0; i<response.body().getPokemonList().size();i++){
-                        Pokemon pokemon = response.body().getPokemonList().get(i);
-                        Log.d(DEBUG_POKEMON, "Te elijo a tí: " + pokemon.getName()+ " id: " + pokemon.getId());
-                    }
+//
+                    PokemonResponse pokemonsResponse = response.body();		// Recuperar el cuerpo de la respuesta
+                    List<Pokemon> newPokemons = pokemonsResponse.getPokemonList();	// Almacenar los datos del cuerpo de la respuesta en una lista
+                    pokemonList.addAll(newPokemons);					// Añadir la lista a la lista pokemon
+                    pokemonAdapter.notifyDataSetChanged();				// Indicar al Adapter que se han añadido nuevos elementos y que debe actualizarse
                 }
             }
 
             @Override
             public void onFailure(Call<PokemonResponse> call, Throwable t) {
-
+                Log.d(Constants.DEBUG_POKEMON,"Algo falla amigo"+t.toString());
             }
         });
     }
---
+++ app/src/main/java/com/example/poke_api/ui/PokemonAdapter.java
@@ -0,0 +1,89 @@
+package com.example.poke_api.ui;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.poke_api.R;
+import com.example.poke_api.models.Pokemon;
+
+import java.util.List;
+
+public class PokemonAdapter extends RecyclerView.Adapter<PokemonAdapter.ViewHolder>{
+
+    //Variables locales
+    private final List<Pokemon> pokemonList;
+    private Context context;
+
+    public PokemonAdapter(List<Pokemon> pokemonList, Context context) {
+        this.pokemonList = pokemonList;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public PokemonAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Creamos vista que se encarge de inflar el item en el ViewHolder
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PokemonAdapter.ViewHolder holder, int position) {
+        holder.bind(pokemonList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return pokemonList.size(); //Devolver elementos de la lista
+    }
+
+    // Clase ViewHolder implementada de forma interna al Adapter
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // Objetos de referencia para las vistas
+
+        TextView tvName, tvId, tvUrl;
+        ImageView ivPokemon;
+        // Asociar objetos a vistas a través del constructor de ViewHolder
+        ViewHolder(View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvId = itemView.findViewById(R.id.tvId);
+            tvUrl = itemView.findViewById(R.id.tvUrl);
+            ivPokemon = itemView.findViewById(R.id.ivPokemon);
+        }
+
+        // Función BIND para rellenar las vistas con la info de su item correspondiente
+        void bind (@NonNull final Pokemon pokemon) {
+
+            //Poblamos las vistas con la información recibida de Retrofit
+            tvName.setText(pokemon.getName());
+            tvId.setText(pokemon.getId());
+            tvUrl.setText(pokemon.getUrl());
+
+
+            String urlSprite = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/"+pokemon.getId()+".gif";
+
+// Insertamos imagen desde URL con Glide en el ImageView
+            Glide.with(context)
+                    .load(urlSprite)
+                    .centerCrop()
+                    .error(R.drawable.pokeball_background)
+                    .fitCenter()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(ivPokemon);
+
+        }
+
+    }
+}
